dd8942a9c689934f74b26d755fc7cc720bf54fd1
APARAJITA K SINGH <aparajitaksingh04@gmail.com>
2026-02-02 22:01:23 +0530

    Update project

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Run ONCE when app loads
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      // basic token presence check (backend verification later)
+      setUser({ token });
+    }
+
+    setLoading(false);
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem("token", token);
+    setUser({ token });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// custom hook
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
