b7709b4d51c84022b58d9ba4bb5c88a92312306e
APARAJITA K SINGH <aparajitaksingh04@gmail.com>
2026-02-02 22:26:21 +0530

    Update project

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -4,7 +4,6 @@ import { useAuth } from "../hooks/useAuth";
 const AuthLayout = () => {
   const { user, loading } = useAuth();
 
-
   if (loading) {
     return (
       <div className="min-h-screen flex items-center justify-center">
@@ -19,7 +18,6 @@ const AuthLayout = () => {
 
   return (
     <div className="min-h-screen flex">
-      {/* Left side (Brand / Info) */}
       <div className="hidden md:flex w-1/2 bg-gradient-to-br from-indigo-600 to-purple-700 text-white items-center justify-center">
         <div className="text-center px-10">
           <h1 className="text-4xl font-bold mb-4">AI Resume Builder</h1>
