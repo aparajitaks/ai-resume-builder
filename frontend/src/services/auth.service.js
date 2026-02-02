29fc2010bcfcdbd800e2da3ab83d385ff92daf90
APARAJITA K SINGH <aparajitaksingh04@gmail.com>
2026-02-02 22:37:13 +0530

    Update project

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,11 +1,29 @@
 import api from "./api";
 
-export const loginUser = async (credentials) => {
-  const res = await api.post("/auth/login", credentials);
+const login = async (data) => {
+  const res = await api.post("/auth/login", data);
+  localStorage.setItem("user", JSON.stringify(res.data));
   return res.data;
 };
 
-export const registerUser = async (data) => {
+const register = async (data) => {
   const res = await api.post("/auth/register", data);
+  localStorage.setItem("user", JSON.stringify(res.data));
   return res.data;
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : null;
+};
+
+export default {
+  login,
+  register,
+  logout,
+  getCurrentUser,
 };
