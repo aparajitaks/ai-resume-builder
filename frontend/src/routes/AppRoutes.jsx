9ee262017fec8b8a6c80d800fabeaf23d46ab1f2
APARAJITA K SINGH <aparajitaksingh04@gmail.com>
2026-02-02 22:07:35 +0530

    Update project

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-// layouts
+import ProtectedRoute from "../components/ProtectedRoute";
+
 import AuthLayout from "../layouts/AuthLayout";
 import DashboardLayout from "../layouts/DashboardLayout";
 
-// pages
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
@@ -13,20 +13,24 @@ import Builder from "../pages/Builder";
 const AppRoutes = () => {
   return (
     <Routes>
-      {/* ---------- AUTH ROUTES ---------- */}
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Route>
 
-      {/* ---------- DASHBOARD / PROTECTED ROUTES ---------- */}
-      <Route element={<DashboardLayout />}>
+      <Route
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/builder" element={<Builder />} />
       </Route>
 
-      {/* ---------- DEFAULT REDIRECT ---------- */}
-      <Route path="*" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
 };
