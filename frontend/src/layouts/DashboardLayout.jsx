bd35051a8572cf61ffa22bd0f1c148c37a33c4f4
APARAJITA K SINGH <aparajitaksingh04@gmail.com>
2026-02-02 22:39:31 +0530

    Update project

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,14 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 import Navbar from "../components/Navbar";
 
 const DashboardLayout = () => {
-  return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
-      {/* Top Navbar */}
-      <Navbar />
+  const { user, loading } = useAuth();
 
-      {/* Main content */}
-      <main className="flex-1 max-w-7xl mx-auto px-4 py-6 w-full">
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1 bg-gray-50 p-6">
         <Outlet />
       </main>
     </div>
